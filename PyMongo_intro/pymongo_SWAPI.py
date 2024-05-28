5992bc2369f131ba0580274701ba4f4435ff2f39
James O'Brien <james@toast.ws>
2024-05-28 08:57:49 +0100

    Fix SWAPI python file to produce all starships from the API

--- PyMongo_intro/pymongo_SWAPI.py
+++ PyMongo_intro/pymongo_SWAPI.py
@@ -1,20 +1,19 @@
 import requests
-import json
+# import json
 import pymongo
 
 client = pymongo.MongoClient()
 db = client.starwars
-db.temp_test.drop()
-db.create_collection("temp_test")
+db.starships.drop()
+db.create_collection("starships")
 
-
-
-url = f'https://swapi.dev/api/starships/?page=4'
-response = requests.get(url)
-sw_list_full = json.loads(response.text)
-sw_list = response.json()['results']
+# url = f'https://swapi.dev/api/starships/?page=4'
+# response = requests.get(url)
+# sw_list_full = json.loads(response.text)
+# sw_list = response.json()['results']
 # print(sw_list_full)
 # print(response.json()['next'])
+
 
 def starship_info(starship_name):
     page_num = 1
@@ -44,15 +43,16 @@ def starship_info(starship_name):
                 pilots_list = (ship_names['pilots'])
                 ship_check = True
                 # print(pilots_list)
-        if response.json()['next'] == None:
+        if response.json()['next'] is None:
             next_page = False
         else:
             page_num += 1
             url = f'https://swapi.dev/api/starships/?page={page_num}'
             response = requests.get(url)
-            sw_list = response.json()['results']
+            response_data = response.json()
+            sw_list = response_data['results']
 
-    if ship_check == False:
+    if ship_check is False:
         #
         return "No such ship."
 
@@ -72,11 +72,10 @@ def starship_info(starship_name):
             'name': a_pilot
         },
             {'name': 1})['_id']
-        #print(pilot_objid)
         id_list.append(pilot_objid)
-    #print(id_list)
-    
+
     return id_list
+
 
 def add_starship(starship_name):
 
@@ -89,14 +88,14 @@ def add_starship(starship_name):
     while next_page:
         url_2 = f'https://swapi.dev/api/starships/?page={page_num_b}'
         response_2 = requests.get(url_2)
-        print(response_2.json())
-        sw_list_2 = response_2.json()['results']
-
+        response_data = response_2.json()
+        print(response_data)
+        sw_list_2 = response_data['results']
 
         print(id_list)
-        #db.temp_test.insert_one({'pilot': id_list})
-        #what_was_added = db.temp_test.find_one({'pilot': id_list})
-        #print(what_was_added)
+        # db.temp_test.insert_one({'pilot': id_list})
+        # what_was_added = db.temp_test.find_one({'pilot': id_list})
+        # print(what_was_added)
 
         for ship in sw_list_2:
             if ship['name'] == starship_name:
@@ -107,7 +106,7 @@ def add_starship(starship_name):
                 # ship_maspeed =
                 # ship_crew
                 # ship_pass
-                db.temp_test.insert_one({
+                db.starships.insert_one({
                     'name': ship['name'],
                     'model': ship['model'],
                     'manufacturer': ship['manufacturer'],
@@ -121,66 +120,68 @@ def add_starship(starship_name):
                 })
                 print("Passed")
                 return
+        else:
+            if response_data['next'] is None:
+                next_page = False
             else:
-                if response_2.json()['next'] == None:
-                    next_page = False
-                else:
-                    page_num_b += 1
-
-
-
+                page_num_b += 1
 
 
 def all_starships():
-    for ship_names in sw_list:
-        pilot_names = []
-            # print(f"Information on {starship_name}:")
-            # print(ship_names['url'])
-            # print(ship_names['model'])
-            # print(ship_names['manufacturer'])
-            # print(ship_names['length'])
-            # print(ship_names['max_atmosphering_speed'])
-            # print(ship_names['crew'])
-            # print(ship_names['passengers'])
-            # print(ship_names['pilots'])
+    page_check = True
+    page_num = 1
+    while page_check:
 
-        pilots_list = (ship_names['pilots'])
-        ship_check = True
-        print(pilots_list)
+        url = f'https://swapi.dev/api/starships/?page={page_num}'
+        response = requests.get(url)
+        # sw_list_full = json.loads(response.text)
+        response_data = response.json()
+        sw_list = response_data['results']
+        for ship_names in sw_list:
+            pilot_names = []
+
+            pilots_list = (ship_names['pilots'])
+            # ship_check = True
+            # print(pilots_list)
+
+            for pilot in pilots_list:
+                pilot_url = pilot
+                response_pilot = requests.get(pilot_url)
+                pilot_names.append(response_pilot.json()['name'])
+
+        # print(pilot_names)
+            id_list = []
+            for a_pilot in pilot_names:
+                # print(db.characters.find_one({
+                #     'name': a_pilot
+                # },
+                #     {'name': 1}))
+                pilot_objid = db.characters.find_one({
+                    'name': a_pilot
+                },
+                    {'name': 1})['_id']
+                # print(pilot_objid)
+                id_list.append(pilot_objid)
+            db.starships.insert_one({
+                'name': ship_names['name'],
+                'model': ship_names['model'],
+                'manufacturer': ship_names['manufacturer'],
+                'length': ship_names['length'],
+                'max_atmosphering_speed': ship_names['max_atmosphering_speed'],
+                'crew': ship_names['crew'],
+                'passengers': ship_names['passengers'],
+                'pilot': id_list
+            })
+        else:
+            print("Page complete")
+            if response_data['next'] is None:
+                # page_check = False
+                print("All pages checked")
+                return 0
+            else:
+                page_num += 1
 
 
-        for pilot in pilots_list:
-            pilot_url = pilot
-            response_pilot = requests.get(pilot_url)
-            pilot_names.append(response_pilot.json()['name'])
-
-    # print(pilot_names)
-        id_list = []
-        for a_pilot in pilot_names:
-        # print(db.characters.find_one({
-        #     'name': a_pilot
-        # },
-        #     {'name': 1}))
-            pilot_objid = db.characters.find_one({
-                'name': a_pilot
-            },
-                {'name': 1})['_id']
-            print(pilot_objid)
-            id_list.append(pilot_objid)
-        db.temp_test.insert_one({
-            'name': ship_names['name'],
-            'model': ship_names['model'],
-            'manufacturer': ship_names['manufacturer'],
-            'length': ship_names['length'],
-            'max_atmosphering_speed': ship_names['max_atmosphering_speed'],
-            'crew': ship_names['crew'],
-            'passengers': ship_names['passengers'],
-            'pilot': id_list
-        })
-
-    #print(id_list)
-
-
-print(starship_info('Slave 1'))
-add_starship('Slave 1')
-#all_starships()
+# print(starship_info('Slave 1'))
+# add_starship('Slave 1')
+all_starships()
